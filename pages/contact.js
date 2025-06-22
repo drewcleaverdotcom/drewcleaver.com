1590e069d05c7785e0d967011f9c18fe61309717
Drew Cleaver <196958053+drewcleaverdotcomadmin@users.noreply.github.com>
2025-06-22 15:32:22 -0500

    Add responsive Tailwind navbar

---
+++ pages/contact.js
@@ -0,0 +1,7 @@
+export default function Contact() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-black text-[#ffe717]">
+      <h1 className="text-4xl">Contact Page</h1>
+    </div>
+  );
+}
