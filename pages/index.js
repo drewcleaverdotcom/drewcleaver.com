d249a0e27944991e858d43356602c49c3bf58e3b
Drew Cleaver <196958053+drewcleaverdotcomadmin@users.noreply.github.com>
2025-06-22 15:01:41 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-black">
-      <h1 className="text-4xl font-normal text-[#ffe717]">Welcome to DrewCleaver.com</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-black">
+      <Image
+        src="/DrewCconsultingLOGOcanary.png"
+        alt="DC Consulting Logo"
+        width={300}
+        height={300}
+      />
+      <h1 className="mt-4 text-4xl font-normal text-[#ffe717]">
+        Welcome to DrewCleaver.com
+      </h1>
     </div>
   );
 }
