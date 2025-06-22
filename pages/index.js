007f53d717d63a3d7dbb08eef0b2799302378aa6
Drew Cleaver <196958053+drewcleaverdotcomadmin@users.noreply.github.com>
2025-06-22 15:05:53 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-black gap-120">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-black gap-400">
       <Image
         src="/DrewCconsultingLOGOcanary.png"
         alt="DC Consulting Logo"
