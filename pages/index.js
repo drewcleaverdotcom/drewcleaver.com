3c4d86a14aa0ce4dd8efca33b3956e8122412703
Drew Cleaver <196958053+drewcleaverdotcomadmin@users.noreply.github.com>
2025-06-22 14:50:54 -0500

    Update index.js
    style: update home page colors and fonts.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-800">Welcome to DrewCleaver.com</h1>
+    <div className="flex min-h-screen items-center justify-center bg-black">
+      <h1 className="text-4xl font-normal text-[#ffe717]">Welcome to DrewCleaver.com</h1>
     </div>
   );
 }
